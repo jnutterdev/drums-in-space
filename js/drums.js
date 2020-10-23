c56ccb72fd80185e64c76f9bfcf311c1717575d0
terribledevice <longlost@gmail.com>
2020-10-22 21:44:18 -0400

    still can't get click to work

--- js/drums.js
+++ js/drums.js
@@ -1,21 +1,34 @@
+// Function for setting variables & playing the sounds
+
 function playSound(e) {
+
     const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
+
     if (!audio) return;
+
     audio.currentTime = 0;
     audio.play();
     key.classList.add('playing');
 }
 
+function clickSound(f) {
+
+    const clicked = document.querySelector(`audio[data-key="${f.keyCode}"]`);
+
+    clicked.play();
+}
 
 
 function removeTransition(e) {
+
     if (e.propertyName !== 'transform') return;
     this.classList.remove('playing');
 }
 
+
+
 const keys = document.querySelectorAll('.key');
-
 keys.forEach(key => key.addEventListener('transitionend', removeTransition));
-
-window.addEventListener('keydown', playSound);
+document.addEventListener('keydown', playSound);
+document.addEventListener('click', clickSound);
