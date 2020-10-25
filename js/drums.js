66c4639f39a389ac0872fec3cb7f85bed1a271ee
terribledevice <longlost@gmail.com>
2020-10-25 14:00:59 -0400

    added clickable sounds

--- js/drums.js
+++ js/drums.js
@@ -1,51 +1,73 @@
 function playSound(e) {
-
-    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     const key = document.querySelector(`.key[data-key="${e.keyCode}"]`);
-
+    const audio = document.querySelector(`audio[data-key="${e.keyCode}"]`);
     if (!audio) return;
     audio.currentTime = 0;
-
     audio.play();
     key.classList.add('playing');
 }
 
-function clickedSound() {
-    const kick = document.querySelector('#kick');
-    const hihat = document.getElementById('hihat');
-    const clap = document.getElementById('clap');
-    const snare = document.getElementById('snare');
-    const cowbell = document.getElementById('cowbell');
-    const tom = document.getElementById('tom');
-    const zap = document.getElementById('zap');
-    const crash = document.getElementById('crash');
-    const laser = document.getElementById('laser');
-
-
-    if (!kick && !hihat && !crash && !clap && !snare && !cowbell && !tom && !zap && !laser) return;
+function kickDrum() {
+    const kick = document.getElementById('kick');
     kick.currentTime = 0;
-    hihat.currentTime = 0;
-    crash.currentTime = 0;
-    clap.currentTime = 0;
-    zap.currentTime = 0;
-    laser.currentTime = 0;
+    if (!kick) return;
+    kick.play();
+}
+
+function hihatDrum() {
+    const hihat = document.getElementById('hihat');
+    kick.currentTime = 0;
+    if (!hihat) return;
+    hihat.play();
+}
+
+function snareDrum() {
+    const snare = document.getElementById('snare');
     snare.currentTime = 0;
+    if (!snare) return;
+    snare.play();
+}
+
+function crashDrum() {
+    const crash = document.getElementById('crash');
+    crash.currentTime = 0;
+    if (!crash) return;
+    crash.play();
+}
+
+function clapdrum() {
+    const clap = document.getElementById('clap');
+    clap.currentTime = 0;
+    if (!clap) return;
+    clap.play();
+}
+
+function tomDrum() {
+    const tom = document.getElementById('tom');
     tom.currentTime = 0;
+    if (!tom) return;
+    tom.play();
+}
+
+function cowbellDrum() {
+    const cowbell = document.getElementById('cowbell');
     cowbell.currentTime = 0;
+    if (!cowbell) return;
+    cowbell.play();
+}
 
-    // kick.play();
-    // hihat.play();
-    // clap.play();
-    // snare.play();
-    // cowbell.play();
-    // tom.play();
-    // zap.play();
-    // crash.play();
-    // laser.play();
+function zapDrum() {
+    const zap = document.getElementById('zap');
+    zap.currentTime = 0;
+    if (!zap) return;
+    zap.play();
+}
 
-
-    key.classList.add('playing');
-
+function laserDrum() {
+    const laser = document.getElementById('laser');
+    laser.currentTime = 0;
+    if (!laser) return;
+    laser.play();
 }
 
 
