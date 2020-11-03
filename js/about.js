af056e9040387940e16816bc11810ec306261c3b
Tara Davies <taradavies@Taras-Air.attlocal.net>
2020-11-02 22:25:33 -0500

    put the fetch in the wrong api, fixing in this commit

--- js/about.js
+++ js/about.js
@@ -1,7 +1,7 @@
 
 //pull the data from the API
 
-fetch('http://api.open-notify.org/astros.json') 
+fetch('https://cors-anywhere.herokuapp.com/https://admiring-brown-29c503.netlify.app/astros.json') 
     .then((response) => response.json()) // translate data to Json
     .then((data) => { 
         document.getElementById('space-fact').innerHTML = (renderPeopleInSpaceFacts(data)); // display the space facts 
