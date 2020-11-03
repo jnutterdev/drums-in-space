bcb40ac751947c79ca125ea08909739688b951c5
terribledevice <longlost@gmail.com>
2020-11-03 16:49:35 -0500

    added hilights to 4th and 8th drum pad

--- js/visual.js
+++ js/visual.js
@@ -1,4 +1,3 @@
-
 function setup() {
 
     var canvas = createCanvas(2000, 170);
@@ -9,22 +8,21 @@ function setup() {
     fill(100)
     stroke(500)
     createLoop({
-        gif: {fileName: "noiseLoop1d.gif", render: false},
-        noiseRadius: 0.2 
-        
+        gif: { fileName: "noiseLoop1d.gif", render: false },
+        noiseRadius: 0.2
+
     })
 
 }
 
 function draw() {
-    background(26, 31, 36); 
-    translate(5, height/1.5);
+    background(26, 31, 36);
+    translate(5, height / 1.5);
     const distributionFrequency = 0.02
     for (let x = 0; x < width; x++) {
-        const y = animLoop.noise1D(x * distributionFrequency) * height /2.5
+        const y = animLoop.noise1D(x * distributionFrequency) * height / 2.5
         ellipse(x, y, 3)
     }
- 
-    
-}
 
+
+}
